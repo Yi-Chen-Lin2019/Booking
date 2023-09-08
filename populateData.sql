e43e2b31b530b71e3d7e6bad0ab6464031137192
Yi-Chen-Lin2019 <58112802+Yi-Chen-Lin2019@users.noreply.github.com>
2023-09-08 11:33:28 +0200

    remove populate data script since it's not needed anymore

--- populateData.sql
+++
@@ -1,4 +0,0 @@
-﻿INSERT INTO Booking ([CreatedDate], [OutboundDate], [ReturnDate], [From], [To]) OUTPUT INSERTED.Id VALUES ('20081111', '20081111', '20081112', 'Aalborg', 'Copenhagen')
-
-SELECT b.Id,[CreatedDate], [OutboundDate], [ReturnDate], [From], [To], [FirstName], [MiddleName], [LastName], [Country], [PassportNo], [ExpireDate] FROM Booking b INNER JOIN Passenger p on p.BookingId = b.Id
-SELECT b.*, p.* FROM Booking b INNER JOIN Passenger p on p.BookingId = b.Id
